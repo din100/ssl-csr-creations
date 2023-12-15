20c2311f0fdaf72c2f4eae39badc7f5210076773
din100 <din100@gmail.com>
2023-12-15 18:06:09 +0000

    added dir

--- csr.sh
+++ csr.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "CSR creator"
 read -p "What is your domain name you want to do the CSR " domainname
+mkdir $domainname
+cd $domainname
 openssl req -new -newkey rsa:2048 -nodes -keyout $domainname.key -out $domainname.csr
